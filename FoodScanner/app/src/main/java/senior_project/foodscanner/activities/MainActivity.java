4963635cdb2499000d299982d3831bf3a09032de
Daniel Alonso <daniel.rodrigo.alonso@gmail.com>
2015-09-18 21:37:54 -0400

    Stable Commit

--- FoodScanner/app/src/main/java/senior_project/foodscanner/activities/MainActivity.java
+++ FoodScanner/app/src/main/java/senior_project/foodscanner/activities/MainActivity.java
@@ -1,39 +1,34 @@
 package senior_project.foodscanner.activities;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import senior_project.foodscanner.R;
 
 
-//TEST
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        int wait = 3000;// wait this many milliseconds before moving on to next activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               // startActivity(new Intent(MainActivity.this, MealDetails.class));
+                finish();
+            }
+        }, wait);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
