b31fcdeacc7c2effcafebe5275ef83a8f3d74f07
tragessere <tragessere@gmail.com>
2015-09-18 21:35:07 -0400

    Added LoginActivity.
    Layout and some animations for the login screen.

---
+++ FoodScanner/app/src/main/java/senior_project/foodscanner/activities/LoginActivity.java
@@ -0,0 +1,260 @@
+package senior_project.foodscanner.activities;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Interpolator;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
+
+import senior_project.foodscanner.R;
+
+/**
+ * Created by Evan on 9/16/2015.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+
+	Button loginButton;
+	Button googleButton;
+	Button facebookButton;
+	EditText emailEdit;
+	TextInputLayout emailHolder;
+	EditText passwordEdit;
+	TextInputLayout passwordHolder;
+
+	Rect loginButtonStartBounds;
+
+	//Disable actions when this is true;
+	boolean isProcessing = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_activity);
+
+		loginButton = (Button) findViewById(R.id.login_food_scanner_button);
+		googleButton = (Button) findViewById(R.id.login_google_plus_button);
+		facebookButton = (Button) findViewById(R.id.login_facebook_button);
+		emailEdit = (EditText) findViewById(R.id.email_text);
+		emailHolder = (TextInputLayout) findViewById(R.id.email_text_holder);
+		passwordEdit = (EditText) findViewById(R.id.password_text);
+		passwordHolder = (TextInputLayout) findViewById(R.id.password_text_holder);
+		Button createAccountButton = (Button) findViewById(R.id.create_account_button);
+		Button recoverPasswordButton = (Button) findViewById(R.id.recover_password_button);
+
+//		facebookButton.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//				Toast.makeText(LoginActivity.this, "test button", Toast.LENGTH_SHORT).show();
+//
+//			}
+//		});
+
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if(emailHolder.getVisibility() == View.VISIBLE) {
+					//Do something
+				} else {
+					showLoginText();
+				}
+			}
+		});
+
+		createAccountButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Rect temp = new Rect();
+				loginButton.getGlobalVisibleRect(temp);
+				Log.d("login button location", temp.toString());
+			}
+		});
+
+	}
+
+
+	@Override
+	public void onBackPressed() {
+		if(emailHolder.getVisibility() == View.VISIBLE)
+			hideLoginText();
+		else
+			super.onBackPressed();
+	}
+
+	private void showLoginText() {
+//		Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+//		Point size = new Point();
+//		display.getSize(size);
+//		final int screenHeight = size.y;
+
+		Rect statusBarBorder = new Rect();
+		getWindow().getDecorView().getWindowVisibleDisplayFrame(statusBarBorder);
+		int statusBarHeight = statusBarBorder.top;
+
+
+		if(loginButtonStartBounds == null) {
+			loginButtonStartBounds = new Rect();
+			loginButton.getGlobalVisibleRect(loginButtonStartBounds);
+			loginButtonStartBounds.top -= statusBarHeight;
+			loginButtonStartBounds.bottom -= statusBarHeight;
+		}
+
+
+		Rect endBounds = new Rect();
+		facebookButton.getGlobalVisibleRect(endBounds);
+		endBounds.top -= statusBarHeight;
+		endBounds.bottom -= statusBarHeight;
+
+		ObjectAnimator translate = ObjectAnimator.ofFloat(loginButton, View.TRANSLATION_Y, endBounds.top - loginButtonStartBounds.top);
+		translate.setInterpolator(new DecelerateInterpolator());
+		translate.setDuration(250);
+
+		final ObjectAnimator fadeTextOut = ObjectAnimator.ofInt(loginButton, "textColor", Color.WHITE, Color.TRANSPARENT);
+		fadeTextOut.setEvaluator(new ArgbEvaluator());
+		fadeTextOut.setDuration(125);
+		fadeTextOut.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				super.onAnimationEnd(animation);
+				loginButton.setText(getString(R.string.login));
+				fadeTextOut.removeAllListeners();
+			}
+		});
+		ObjectAnimator fadeTextIn = ObjectAnimator.ofInt(loginButton, "textColor", Color.TRANSPARENT, Color.WHITE);
+		fadeTextIn.setEvaluator(new ArgbEvaluator());
+		fadeTextIn.setDuration(125);
+		fadeTextIn.setStartDelay(125);
+
+
+		final ObjectAnimator fadeEmailIn = ObjectAnimator.ofFloat(emailHolder, View.ALPHA, 0, 1);
+		fadeEmailIn.setDuration(200);
+		fadeEmailIn.setStartDelay(100);
+		fadeEmailIn.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationStart(Animator animation) {
+				super.onAnimationStart(animation);
+				emailHolder.setVisibility(View.VISIBLE);
+				passwordHolder.setVisibility(View.VISIBLE);
+			}
+
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				super.onAnimationEnd(animation);
+				emailEdit.setFocusable(true);
+				emailEdit.setFocusableInTouchMode(true);
+				passwordEdit.setFocusable(true);
+				passwordEdit.setFocusableInTouchMode(true);
+
+				fadeEmailIn.removeAllListeners();
+			}
+		});
+		ObjectAnimator fadePasswordIn = ObjectAnimator.ofFloat(passwordHolder, View.ALPHA, 0, 1);
+		fadePasswordIn.setDuration(200);
+		fadePasswordIn.setStartDelay(100);
+
+		final ObjectAnimator googleFadeOut = ObjectAnimator.ofFloat(googleButton, View.ALPHA, 1, 0);
+		googleFadeOut.setDuration(125);
+		googleFadeOut.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationStart(Animator animation) {
+				super.onAnimationStart(animation);
+				googleButton.setClickable(false);
+				facebookButton.setClickable(false);
+				googleFadeOut.removeAllListeners();
+			}
+		});
+		ObjectAnimator facebookFadeOut = ObjectAnimator.ofFloat(facebookButton, View.ALPHA, 1, 0);
+		facebookFadeOut.setDuration(125);
+
+		AnimatorSet set = new AnimatorSet();
+		set.playTogether(translate, fadeTextOut, fadeTextIn, fadeEmailIn, fadePasswordIn, googleFadeOut, facebookFadeOut);
+		set.start();
+	}
+
+
+	private void hideLoginText() {
+		Rect startBounds = new Rect();
+		facebookButton.getGlobalVisibleRect(startBounds);
+
+		ObjectAnimator translate = ObjectAnimator.ofFloat(loginButton, View.TRANSLATION_Y, 0);
+		translate.setInterpolator(new DecelerateInterpolator());
+		translate.setDuration(250);
+
+		final ObjectAnimator fadeTextOut = ObjectAnimator.ofInt(loginButton, "textColor", Color.WHITE, Color.TRANSPARENT);
+		fadeTextOut.setEvaluator(new ArgbEvaluator());
+		fadeTextOut.setDuration(125);
+		fadeTextOut.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				super.onAnimationEnd(animation);
+				loginButton.setText(getString(R.string.login_food_scanner));
+				fadeTextOut.removeAllListeners();
+			}
+		});
+		ObjectAnimator fadeTextIn = ObjectAnimator.ofInt(loginButton, "textColor", Color.TRANSPARENT, Color.WHITE);
+		fadeTextIn.setEvaluator(new ArgbEvaluator());
+		fadeTextIn.setDuration(125);
+		fadeTextIn.setStartDelay(125);
+
+
+		final ObjectAnimator fadeEmailOut = ObjectAnimator.ofFloat(emailHolder, View.ALPHA, 1, 0);
+		fadeEmailOut.setInterpolator(new DecelerateInterpolator());
+		fadeEmailOut.setDuration(125);
+		fadeEmailOut.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationStart(Animator animation) {
+				super.onAnimationStart(animation);
+				emailEdit.setFocusable(false);
+				passwordEdit.setFocusable(false);
+			}
+
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				super.onAnimationEnd(animation);
+				emailHolder.setVisibility(View.INVISIBLE);
+				passwordHolder.setVisibility(View.INVISIBLE);
+
+				fadeEmailOut.removeAllListeners();
+			}
+		});
+		ObjectAnimator fadePasswordOut = ObjectAnimator.ofFloat(passwordHolder, View.ALPHA, 1, 0);
+		fadePasswordOut.setInterpolator(new DecelerateInterpolator());
+		fadePasswordOut.setDuration(125);
+
+		final ObjectAnimator googleFadeIn = ObjectAnimator.ofFloat(googleButton, View.ALPHA, 0, 1);
+		googleFadeIn.setDuration(200);
+		googleFadeIn.setStartDelay(100);
+		googleFadeIn.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationStart(Animator animation) {
+				super.onAnimationStart(animation);
+				googleButton.setClickable(true);
+				facebookButton.setClickable(true);
+				googleFadeIn.removeAllListeners();
+			}
+		});
+		ObjectAnimator facebookFadeIn = ObjectAnimator.ofFloat(facebookButton, View.ALPHA, 0, 1);
+		facebookFadeIn.setDuration(200);
+		facebookFadeIn.setStartDelay(100);
+
+		AnimatorSet set = new AnimatorSet();
+		set.playTogether(translate, fadeTextOut, fadeTextIn, fadeEmailOut, fadePasswordOut, googleFadeIn, facebookFadeIn);
+		set.start();
+	}
+}
