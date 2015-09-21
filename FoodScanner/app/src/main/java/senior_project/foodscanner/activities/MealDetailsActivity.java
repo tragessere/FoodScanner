095b1cfc3a3802ced1bfa329e67265ddba82a37f
Shmoozen <tgrikas@purdue.edu>
2015-09-20 22:44:26 -0400

    Added meal spinner
    Defaults to different meals, based on time of day.

--- FoodScanner/app/src/main/java/senior_project/foodscanner/activities/MealDetailsActivity.java
+++ FoodScanner/app/src/main/java/senior_project/foodscanner/activities/MealDetailsActivity.java
@@ -6,7 +6,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 import senior_project.foodscanner.R;
 
@@ -32,6 +38,20 @@ import senior_project.foodscanner.R;
  */
 public class MealDetailsActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private String[] meals;
+    private Spinner mealSpinner;
+
+    // region Times that meal spinner will start defaulting to each meal (in min, 24-hour format)
+    // TODO: Allow user to set these times
+    public int breakfastTime = 240; // 3:00 (am)
+    public int brunchTime = 600;    // 10:00 (am)
+    public int lunchTime = 720;     // 12:00 (am)
+    public int snackTime = 900;     // 15:00 (3:00pm)
+    public int dinnerTime = 1080;   // 18:00 (6:00pm)
+    public int dessertTime = 1260;  // 21:00 (9:00pm)
+    // endregion
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +59,53 @@ public class MealDetailsActivity extends AppCompatActivity implements View.OnCli
 
         Button button = (Button) findViewById(R.id.button_foodscanner);
         button.setOnClickListener(this);
+
+        // Set up meal selection spinner
+        mealSpinner = (Spinner) findViewById(R.id.spinner_meal);
+        meals = getResources().getStringArray(R.array.meal_list);
+        ArrayAdapter<String> mealAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, meals);
+        mealAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mealSpinner.setAdapter(mealAdapter);
+
+        // Set default meal, based on time of day
+        Calendar cal = Calendar.getInstance();
+        int minute = cal.get(Calendar.MINUTE);
+        int hour = cal.get(Calendar.HOUR_OF_DAY); //24-hour format
+        int time = minute + (hour * 60);
+        String defaultMeal;
+
+        if (time >= breakfastTime && time < brunchTime) {
+            defaultMeal = "Breakfast";
+        } else if (time >= brunchTime && time < lunchTime) {
+            defaultMeal = "Brunch";
+        } else if (time >= lunchTime && time < snackTime) {
+            defaultMeal = "Lunch";
+        } else if (time >= snackTime && time < dinnerTime) {
+            defaultMeal = "Snack";
+        } else if (time >= dinnerTime && time < dessertTime) {
+            defaultMeal = "Dinner";
+        } else {
+            defaultMeal = "Dessert";
+        }
+
+        mealSpinner.setSelection(mealAdapter.getPosition(defaultMeal));
+
+        // Set up what meal selection does
+        mealSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Currently, do nothing. Below was for testing.
+                //String text = mealSpinner.getSelectedItem().toString();
+                //(Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT)).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // Do nothing
+            }
+        });
     }
 
     @Override
@@ -69,4 +136,5 @@ public class MealDetailsActivity extends AppCompatActivity implements View.OnCli
             startActivity(new Intent(MealDetailsActivity.this, FoodScannerActivity.class));
         }
     }
+
 }
