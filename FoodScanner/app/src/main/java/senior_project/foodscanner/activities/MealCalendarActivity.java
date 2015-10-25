5e77fc39d4968ceedfee586055405809195e2215
tragessere <tragessere@gmail.com>
2015-10-25 13:48:03 -0400

    Added Settings Activity.
    Changed the logout button on the calendar page to go to the settings page.
    
    Changed app theme to the dark toolbar so the text will be white.

--- FoodScanner/app/src/main/java/senior_project/foodscanner/activities/MealCalendarActivity.java
+++ FoodScanner/app/src/main/java/senior_project/foodscanner/activities/MealCalendarActivity.java
@@ -89,10 +89,10 @@ public class MealCalendarActivity extends AppCompatActivity implements View.OnCl
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if(id == R.id.action_login) {
+        if(id == R.id.action_settings) {
             //TODO go to login screen here
             //TODO upon returning from logging in, update UI here to indicate user is logged in and has the ability to log out
-            LoginActivity.logout(this);
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
---
+++ FoodScanner/app/src/main/java/senior_project/foodscanner/activities/SettingsActivity.java
@@ -0,0 +1,18 @@
+package senior_project.foodscanner.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import senior_project.foodscanner.R;
+
+/**
+ * Created by Evan on 10/24/2015.
+ */
+public class SettingsActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+	}
+}
