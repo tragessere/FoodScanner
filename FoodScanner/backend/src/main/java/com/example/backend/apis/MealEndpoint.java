4df2ef24d1654a2bb0a77fe3dcae25091bcaade8
mlenarto <mlenarto@purdue.edu>
2015-10-29 12:10:36 -0400

    request meals within two dates endpoint

--- FoodScanner/backend/src/main/java/com/example/backend/apis/MealEndpoint.java
+++ FoodScanner/backend/src/main/java/com/example/backend/apis/MealEndpoint.java
@@ -16,6 +16,7 @@ import com.google.appengine.api.users.User;
 import com.googlecode.objectify.cmd.Query;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Named;
@@ -49,6 +50,19 @@ public class MealEndpoint {
     @ApiMethod(name = "getAllMeals")
     public CollectionResponse<Meal> getAllMeals (User user) throws ServiceException {
         Query<Meal> query = ofy().load().type(Meal.class);
+        List<Meal> results = new ArrayList<Meal>();
+        QueryResultIterator<Meal> iterator = query.iterator();
+
+        while (iterator.hasNext()) {
+            results.add(iterator.next());
+        }
+
+        return CollectionResponse.<Meal>builder().setItems(results).build();
+    }
+
+    @ApiMethod(name = "getMealsWithinDates")
+    public CollectionResponse<Meal> getMealsWithinDates (@Named("startDate")Date startDate, @Named("endDate")Date endDate, User user) throws ServiceException {
+        Query<Meal> query = ofy().load().type(Meal.class).filter("date >=", startDate).filter("date <=", endDate);
         List<Meal> results = new ArrayList<Meal>();
         QueryResultIterator<Meal> iterator = query.iterator();
 
