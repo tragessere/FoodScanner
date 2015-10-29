70a84c270bdf5299882983c47e29dc24cb0ebd4f
tragessere <tragessere@gmail.com>
2015-10-29 19:54:33 -0400

    Updated Settings Activity.
    Added more options to the UI and implemented some touch effects.

--- FoodScanner/app/src/main/java/senior_project/foodscanner/activities/SettingsActivity.java
+++ FoodScanner/app/src/main/java/senior_project/foodscanner/activities/SettingsActivity.java
@@ -2,6 +2,9 @@ package senior_project.foodscanner.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.view.MenuItem;
+import android.view.View;
 
 import senior_project.foodscanner.R;
 
@@ -14,5 +17,38 @@ public class SettingsActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+
+		View hourFormat = findViewById(R.id.hour_format_layout);
+		final SwitchCompat hourSwitch = (SwitchCompat) findViewById(R.id.hour_format_switch);
+		View logout = findViewById(R.id.logout_layout);
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+		hourFormat.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				hourSwitch.setChecked(!hourSwitch.isChecked());
+			}
+		});
+
+		logout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				LoginActivity.logout(SettingsActivity.this);
+			}
+		});
+	}
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				onBackPressed();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 }
